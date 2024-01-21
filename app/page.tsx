655c7ff34123f536cee44fd78186f28355ab5ca1
unknown <j.canedo.527294@umindanao.edu.ph>
2024-01-21 12:46:00 +0800

    Dashboard setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import Link from "next/link";
-
-export default function Home() {
+import { Sidepanel } from "@/components";
+export default function Dashboard() {
   return (
-    <div className="bg-custom-white w-full h-screen flex p-2">
-      <div className="sticky top-0 left-0 h-full bg-primary-green rounded-lg">
-        <ul className="text-[30px] flex justify-center">
-          <li className="hover:bg-green-600 px-5 py-2 border-custom-white mt-3 border-y-[1px] cursor-pointer">
-            # Dashboard
-          </li>
-        </ul>
-      </div>
+    <div className="bg-custom-white w-full h-screen flex p-2 ">
+      <Sidepanel />
       <h1 className="text-black">Hello</h1>
       <Link href="/test">Test</Link>
     </div>
