fcf334ef686dc12bf9f11bcb9fba7e16e139ec17
whoisbel <mjabelcina@gmail.com>
2024-01-21 22:50:00 +0800

    Sidepanel improved installed react-icons

--- components/Sidepanel.tsx
+++ components/Sidepanel.tsx
@@ -1,14 +1,32 @@
-import dashboardIcon from '../public/dashboard-icon.svg'
-import Image from 'next/image';
+'use client'
+import { LuLayoutDashboard } from 'react-icons/lu';
+import Link from 'next/link';
+import { TfiWrite } from "react-icons/tfi";
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+
 const Sidepanel = () => {
+  const pathname = usePathname()
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    setIsClicked((prevIsClicked) => !prevIsClicked);
+  };
+  // active:bg-main-background active:text-letters-color
   return (
     <div className="sticky top-0 left-0 h-full bg-primary-green rounded-lg hidden md:block bg-primary-color">
-      <ul className="text-[30px] flex justify-center">
-      <div className="flex hover:bg-main-background hover:text-letters-color items-center justify-center mt-4 p-2.5">
-        <Image src={dashboardIcon} alt="icon" width={50} height={30} className='fill-slate-800'/>
-        <li className="p-2 cursor-pointer self-center">
+      <ul className="text-[30px] flex flex-col">
+      <div className={isClicked ? 'flex items-center justify-center mt-4 p-2.5 bg-main-background text-letters-color' : 'flex items-center justify-center mt-4 p-2.5 bg-primary-color'} onClick={handleClick}>
+        <LuLayoutDashboard className={isClicked ? 'text-primary-color w-11 h-11' : 'text-main-background w-11 h-11'}/>
+        <Link className="p-2 cursor-pointer self-center font-bold" href="/">
           Dashboard
-        </li>
+        </Link>
+      </div>
+      <div className={isClicked ? 'flex items-center justify-center mt-4 p-2.5 bg-main-background text-letters-color' : 'flex items-center justify-center mt-4 p-2.5 bg-primary-color'} onClick={handleClick}>
+        <TfiWrite className={isClicked ? 'text-primary-color w-11 h-11' : 'text-main-background w-11 h-11'}/>
+        <Link className="p-2 cursor-pointer self-center font-bold" href="/">
+          Inventory Input
+        </Link>
       </div>
       </ul>
     </div>
