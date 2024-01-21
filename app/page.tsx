f1c4106fefeea56fcc51c27be900fed0b04b9049
whoisbel <mjabelcina@gmail.com>
2024-01-21 21:26:38 +0800

    tailwindcss added color scheme added dashboard icon changes in sidepanel.tsx and page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@ import Link from "next/link";
 import { Sidepanel } from "@/components";
 export default function Dashboard() {
   return (
-    <div className="bg-custom-white w-full h-screen flex p-2 ">
+    <div className="bg-main-background w-full h-screen flex p-2 ">
       <Sidepanel />
       <h1 className="text-black">Hello</h1>
-      <Link href="/test">Test</Link>
     </div>
   );
 }
