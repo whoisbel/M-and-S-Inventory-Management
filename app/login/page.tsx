2f90d7640ff7b2eb3b5b141a0334be13b4b84565
whoisbel <mjabelcina@gmail.com>
2024-01-22 23:30:25 +0800

    Updated font size in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,9 +8,9 @@ const Login = () => {
     return <div className="h-full w-full bg-main-background text-black flex justify-center items-center">
                 <div className="w-[350px] h-[338px] bg-custom-white rounded-3xl flex flex-col items-center justify-center pl-6 pr-6 pt-8 pb-8">
                     <h1 className="text-[41px] font-bold mb-[22px]">M&S Company</h1>
-                    <input type="text" className="bg-main-background rounded-3xl w-[300px] h-[50px] mb-[22px] pl-4" placeholder="ID"/>
-                    <input type="password" className="bg-main-background rounded-3xl w-[300px] h-[50px] mb-[22px] pl-4" placeholder="Password"/>
-                    <button className="w-[300px] h-[50px] text-custom-white bg-primary-color rounded-3xl">Login</button>
+                    <input type="text" className="bg-main-background rounded-3xl w-[300px] h-[50px] mb-[22px] pl-4 text-2xl" placeholder="ID"/>
+                    <input type="password" className="bg-main-background rounded-3xl w-[300px] h-[50px] mb-[22px] pl-4 text-2xl" placeholder="Password"/>
+                    <button className="w-[300px] h-[50px] text-custom-white bg-primary-color rounded-3xl font-bold text-[33px]">Login</button>
                 </div>
             </div>;
 };
