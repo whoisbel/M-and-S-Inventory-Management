760f5e852a4839cdc0d599e8260c97efcc87b915
kjdutaro <kjdutaro@gmail.com>
2024-02-02 02:07:02 +0800

    updated database

--- database/db.sql
+++ database/db.sql
@@ -2,9 +2,26 @@ CREATE DATABASE IF NOT EXISTS mns_inventory;
 
 USE mns_inventory;
 
-CREATE TABLE IF NOT EXISTS Users (
-    user_id VARCHAR PRIMARY KEY,
-    password VARCHAR NOT NULL);
+CREATE TABLE IF NOT EXISTS Codes(
+    access_code INT PRIMARY KEY
+)
+
+CREATE TABLE IF NOT EXISTS Role(
+    role_id INT PRIMARY KEY AUTOINCREMENT,
+    description VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS Personnel (
+    personnel_id INT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    username VARCHAR(255) NOT NULL,
+    role int NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    hasAccess BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (role_id) REFERENCES Role (role_id)
+    );
 
 CREATE TABLE IF NOT EXISTS Area(
     area_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -33,6 +50,7 @@ CREATE TABLE IF NOT EXISTS Customer(
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NULL,
     mobile VARCHAR(255) NULL,
+    isDeleted BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS HarvestLog(
@@ -40,6 +58,7 @@ CREATE TABLE IF NOT EXISTS HarvestLog(
     area_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     harvestDate DATETIME,
+    isDeleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (area_id) REFERENCES Area(area_id)
 );
 
@@ -48,6 +67,7 @@ CREATE TABLE IF NOT EXISTS Stock(
     area_id INT NOT NULL,
     grade_id INT NOT NULL,
     QuantityOnHand DOUBLE NOT NULL,
+    isDeleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (area_id) REFERENCES Area(area_id),
     FOREIGN KEY (grade_id) REFERENCES Grade(grade_id)
 );
@@ -60,6 +80,7 @@ CREATE TABLE IF NOT EXISTS Inventory(
     isOutOfStock BOOLEAN DEFAULT FALSE,
     isWashed BOOLEAN DEFAULT FALSE,
     quantity DOUBLE NOT NULL,
+    isDeleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (batch_id) REFERENCES HarvestLog(batch_id),
     FOREIGN KEY (stock_id) REFERENCES Stock(stock_id),
     FOREIGN KEY (grade_id) REFERENCES Grade(grade_id)
@@ -71,6 +92,7 @@ CREATE TABLE IF NOT EXISTS Stockout(
     quantity DOUBLE NOT NULL,
     DateOut DATETIME NOT NULL,
     type_id INT NOT NULL,
+    isDeleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (stock_id) REFERENCES Stock(stock_id),
     FOREIGN KEY (type_id) REFERENCES stockout_type(type_id)
 );
@@ -82,6 +104,7 @@ CREATE TABLE IF NOT EXISTS Order(
     orderDate DATETIME NOT NULL,
     orderTotal DOUBLE NOT NULL,
     isFulfilled BOOLEAN DEFAULT FALSE,
+    isDeleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
@@ -94,11 +117,14 @@ CREATE TABLE IF NOT EXISTS OrderDetails(
     orderTotal DOUBLE NOT NULL,
     LOADING SCHEDULE DATE NULL,
     status_id INT NOT NULL,
+    isDeleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (order_id) REFERENCES Order(order_id),
     FOREIGN KEY (stock_id) REFERENCES Stock(stock_id),
     FOREIGN KEY (status_id) REFERENCES Status(status_id)
 );
 
+INSERT INTO Codes VALUES (0123);
+INSERT INTO Role (description) VALUES ('Admin', 'Employee');
 INSERT INTO stockout_type (description) VALUES ('Ordered'), ('Disposed');
 INSERT INTO Status (description) VALUES ('Processing'), ('Packed'), ('Scheduled for Loading'), ('Fulfilled');
 
--- database/dump-data.sql
+++ database/dump-data.sql
@@ -2,81 +2,78 @@ INSERT INTO Area (description) VALUES ('Area 1'), ('Area 2'), ('Area 3'), ('Area
 
 INSERT INTO Grade (description, price) VALUES ('A', 90), ('B', 80), ('C', 70), ('D', 60), ('Ungraded');
 
-INSERT INTO Users (user_id, password)
+INSERT INTO Personnel (first_name, last_name, middle_name, username, role,  password, hasAccess)
 VALUES
-    ('AAD1234', 'securepassword1'),
-    ('BBC5678', 'strongpass789'),
-    ('CCC9101', 'safeandstrong23'),
-    ('DDD1122', 'password4321'),
-    ('EEE3344', 'myp@ssword567');
+    ('Admin J', 'admin', '', 'admin', 1, 'admin', TRUE),
+    ('Employee J', 'emp', '', 'employee', 2, 'employee', TRUE);
 
-INSERT INTO Customer (first_name, last_name, email, mobile)
+INSERT INTO Customer (first_name, last_name, email, mobile, isDeleted)
 VALUES
-    ('Juan', 'Dela Cruz', 'juan.delacruz@example.com', '+639171234567'),
-    ('Maria', 'Santos', 'maria.santos@example.com', '+639281234567'),
-    ('Jose', 'Reyes', 'jose.reyes@example.com', '+639351234567'),
-    ('Sofia', 'Lim', 'sofia.lim@example.com', '+639461234567'),
-    ('Miguel', 'Cruz', 'miguel.cruz@example.com', '+639571234567'),
-    ('Isabella', 'Gonzales', 'isabella.gonzales@example.com', '+639681234567'),
-    ('Andres', 'Ramos', 'andres.ramos@example.com', '+639791234567'),
-    ('Julia', 'Perez', 'julia.perez@example.com', '+639801234567'),
-    ('Diego', 'Tan', 'diego.tan@example.com', '+639911234567'),
-    ('Angela', 'Garcia', 'angela.garcia@example.com', '+639221234567');
+    ('Juan', 'Dela Cruz', 'juan.delacruz@example.com', '+639171234567', FALSE),
+    ('Maria', 'Santos', 'maria.santos@example.com', '+639281234567', FALSE),
+    ('Jose', 'Reyes', 'jose.reyes@example.com', '+639351234567', FALSE),
+    ('Sofia', 'Lim', 'sofia.lim@example.com', '+639461234567', FALSE),
+    ('Miguel', 'Cruz', 'miguel.cruz@example.com', '+639571234567', FALSE),
+    ('Isabella', 'Gonzales', 'isabella.gonzales@example.com', '+639681234567', FALSE),
+    ('Andres', 'Ramos', 'andres.ramos@example.com', '+639791234567', FALSE),
+    ('Julia', 'Perez', 'julia.perez@example.com', '+639801234567', FALSE),
+    ('Diego', 'Tan', 'diego.tan@example.com', '+639911234567', FALSE),
+    ('Angela', 'Garcia', 'angela.garcia@example.com', '+639221234567', FALSE);
 
-INSERT INTO HarvestLog (area_id, quantity, harvestDate)
+INSERT INTO HarvestLog (area_id, quantity, harvestDate, isDeleted)
 VALUES
-    (1, 100, '2024-01-15 10:00:00'),
-    (2, 150, '2024-01-16 11:30:00'),
-    (3, 120, '2024-01-17 09:45:00'),
-    (4, 200, '2024-01-18 14:20:00'),
-    (5, 180, '2024-01-19 08:00:00');
+    (1, 100, '2024-01-15 10:00:00', FALSE),
+    (2, 150, '2024-01-16 11:30:00', FALSE),
+    (3, 120, '2024-01-17 09:45:00', FALSE),
+    (4, 200, '2024-01-18 14:20:00', FALSE),
+    (5, 180, '2024-01-19 08:00:00', FALSE);
 
-INSERT INTO Inventory (batch_id, stock_id, grade_id, isOutOfStock, isWashed, quantity)
+INSERT INTO Inventory (batch_id, stock_id, grade_id, isOutOfStock, isWashed, quantity, isDeleted)
 VALUES
-    (1, 1, 1, FALSE, TRUE, 80),
-    (1, 1, 2, FALSE, TRUE, 20),
-    (2, 2, 1, FALSE, FALSE, 100),
-    (2, 2, 2, FALSE, FALSE, 50),
-    (3, 3, 3, TRUE, TRUE, 80),
-    (3, 3, 5, TRUE, FALSE, 20),
-    (4, 4, 5, FALSE, FALSE, 200),
-    (5, 5, 5, FALSE, FALSE, 180);
+    (1, 1, 1, FALSE, TRUE, 80, FALSE),
+    (1, 1, 2, FALSE, TRUE, 20, FALSE),
+    (2, 2, 1, FALSE, FALSE, 100, FALSE),
+    (2, 2, 2, FALSE, FALSE, 50, FALSE),
+    (3, 3, 3, TRUE, TRUE, 80, FALSE),
+    (3, 3, 5, TRUE, FALSE, 20, FALSE),
+    (4, 4, 5, FALSE, FALSE, 200, FALSE),
+    (5, 5, 5, FALSE, FALSE, 180, FALSE);
 
-INSERT INTO Stock (area_id, grade_id, QuantityOnHand)
+INSERT INTO Stock (area_id, grade_id, QuantityOnHand, isDeleted)
 VALUES
-    (1, 1, 80),
-    (1, 2, 20),
-    (1, 3, 0),
-    (1, 4, 0),
-    (1, 5, 0),
-    (2, 1, 100),
-    (2, 2, 50),
-    (2, 3, 0),
-    (2, 4, 0),
-    (2, 5, 0),
-    (3, 1, 0),
-    (3, 2, 0),
-    (3, 3, 0),
-    (3, 4, 0),
-    (3, 5, 0),
-    (4, 1, 0),
-    (4, 2, 0),
-    (4, 3, 0),
-    (4, 4, 0),
-    (4, 5, 200),
-    (5, 1, 0),
-    (5, 2, 0),
-    (5, 3, 0),
-    (5, 4, 0),
-    (5, 5, 180);
+    (1, 1, 80, FALSE),
+    (1, 2, 20, FALSE),
+    (1, 3, 0, FALSE),
+    (1, 4, 0, FALSE),
+    (1, 5, 0, FALSE),
+    (2, 1, 100, FALSE),
+    (2, 2, 50, FALSE),
+    (2, 3, 0, FALSE),
+    (2, 4, 0, FALSE),
+    (2, 5, 0, FALSE),
+    (3, 1, 0, FALSE),
+    (3, 2, 0, FALSE),
+    (3, 3, 0, FALSE),
+    (3, 4, 0, FALSE),
+    (3, 5, 0, FALSE),
+    (4, 1, 0, FALSE),
+    (4, 2, 0, FALSE),
+    (4, 3, 0, FALSE),
+    (4, 4, 0, FALSE),
+    (4, 5, 200, FALSE),
+    (5, 1, 0, FALSE),
+    (5, 2, 0, FALSE),
+    (5, 3, 0, FALSE),
+    (5, 4, 0, FALSE),
+    (5, 5, 180, FALSE);
 
 
-INSERT INTO Stockout (stock_id, quantity, DateOut, type_id)
+INSERT INTO Stockout (stock_id, quantity, DateOut, type_id, isDeleted)
 VALUES
-    (13, 80, '2024-01-20 13:30:00', 1),
-    (15, 20, '2024-01-21 12:15:00', 2);
+    (13, 80, '2024-01-20 13:30:00', 1, FALSE),
+    (15, 20, '2024-01-21 12:15:00', 2, FALSE);
 
-INSERT INTO Order (customer_id, isPreOrder, orderDate, orderTotal, isFulfilled)
+INSERT INTO Order (customer_id, isPreOrder, orderDate, orderTotal, isFulfilled, isDeleted)
 VALUES
     (1, FALSE, '2024-01-25 15:00:00', 4500, FALSE),
     (2, TRUE, '2024-01-26 09:30:00', 4000, FALSE),
@@ -84,10 +81,10 @@ VALUES
     (4, TRUE, '2024-01-28 11:45:00', 4500, FALSE),
     (5, FALSE, '2024-01-29 10:00:00', 3500, FALSE);
 
-INSERT INTO OrderDetails (order_id, stock_id, isFulfilled, orderQuantity, orderTotal, LOADING_SCHEDULE, status_id)
+INSERT INTO OrderDetails (order_id, stock_id, isFulfilled, orderQuantity, orderTotal, LOADING_SCHEDULE, status_id, isDeleted)
 VALUES
-    (1, 1, FALSE, 50, 4500, '2024-02-01', 1),
-    (2, 2, FALSE, 50, 4000, '2024-02-02', 1),
-    (1, 3, FALSE, 50, 4500, '2024-02-03', 1),
-    (1, 4, FALSE, 50, 4500, '2024-02-04', 1),
-    (3, 5, FALSE, 50, 3500, '2024-02-05', 1);
+    (1, 1, FALSE, 50, 4500, '2024-02-01', 1, FALSE),
+    (2, 2, FALSE, 50, 4000, '2024-02-02', 1, FALSE),
+    (1, 3, FALSE, 50, 4500, '2024-02-03', 1, FALSE),
+    (1, 4, FALSE, 50, 4500, '2024-02-04', 1, FALSE),
+    (3, 5, FALSE, 50, 3500, '2024-02-05', 1, FALSE);
