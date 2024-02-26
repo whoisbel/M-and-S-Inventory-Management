1d27f898f2eeacf7662d737bd1a4d35aa6e683ac
unknown <j.canedo.527294@umindanao.edu.ph>
2024-02-26 14:03:46 +0800

    stockout update

--- components/inventory/stockout/StockOutModal.tsx
+++ components/inventory/stockout/StockOutModal.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { swalCustomClass } from "@/utils/swalConfig";
 import { CgAdd } from "react-icons/cg";
 import { StockoutForm } from "@/components";
+
 const StockOutModal = ({
   swal,
   setModalOpen,
@@ -16,25 +17,93 @@ const StockOutModal = ({
   grade: Grade[];
 }) => {
   const [remainingStock, setRemainingStock] = useState<Stock>();
-  const [stockouts, setStockouts] = useState<Stock[]>([]);
+  const [stockoutData, setStockoutData] = useState<
+    {
+      stockout: Stockout;
+      error: {
+        quantity: boolean;
+        stock: boolean;
+        grade: boolean;
+        [key: string]: boolean;
+      };
+    }[]
+  >([]);
 
-  const [selectedGrade, setSelectedGrade] = useState(0);
-  const [washed, setWashed] = useState(false);
-
-  const [remainingQuantity, setRemainingQuantity] = useState(0); //The remaining stock after substacted by the input quantity
-  const [isError, setIsError] = useState(false);
-  const [isQuantityError, setIsQuantityError] = useState(false);
+  const [isError, setIsError] = useState({
+    quantity: false,
+    stock: false,
+    grade: false,
+  });
 
   function addStockout() {
-    const stockout: Stockout = {
-      gradeId: grade[0],
+    const newStockout: Stockout = {
+      gradeId: 0,
       quantity: 0,
       washed: false,
     };
-    setStockouts([...stockouts, stockout]);
+    const newError = { quantity: false, stock: false, grade: true };
+    const newData = { stockout: newStockout, error: newError };
+    setStockoutData([...stockoutData, newData]);
   }
-  function removeStockout(index: number) {}
+  function removeStockout(index: number) {
+    const newStockoutData = [...stockoutData];
+    newStockoutData.splice(index, 1);
+    setStockoutData(newStockoutData);
+  }
+  const handleUpdate = (index: number, data: (typeof stockoutData)[0]) => {
+    console.log(data);
+    const newStockoutData = [...stockoutData];
+    newStockoutData[index] = data;
+    setStockoutData(newStockoutData);
+  };
 
+  function getError() {
+    let newError: {
+      quantity: boolean;
+      stock: boolean;
+      grade: boolean;
+      [key: string]: boolean;
+    } = { quantity: false, stock: false, grade: false };
+    stockoutData.forEach((data) => {
+      const { error } = data;
+      Object.keys(error).map((key) => {
+        console.log(newError);
+        if (error[key]) {
+          newError[key] = error[key];
+        }
+      });
+    });
+    console.log({ newError });
+    setIsError(newError);
+  }
+
+  async function handleSubmit() {
+    getError();
+    if (isError.grade || isError.quantity || isError.stock) {
+      return;
+    }
+    const stockouts = stockoutData.map((data) => data.stockout);
+    const response = await fetch("/api/inventory/stockout", {
+      method: "POST",
+      body: JSON.stringify(stockouts),
+    }).then((res) => {
+      if (res.ok) {
+        swal.fire({
+          title: "Stockout added",
+          icon: "success",
+          customClass: swalCustomClass,
+        });
+        setModalOpen(false);
+        location.reload();
+      } else {
+        swal.fire({
+          title: "Error",
+          icon: "error",
+          customClass: swalCustomClass,
+        });
+      }
+    });
+  }
   return (
     <div className="flex bg-white min-w-[900px] min-h-[400px] flex-col text-black rounded">
       <div className=" bg-accent-gray flex items-center p-3">
@@ -50,8 +119,6 @@ const StockOutModal = ({
             defaultValue={new Date().toISOString().split("T")[0]}
           />
 
-          <label>Remaining Quantity:</label>
-          <p className={` ${isQuantityError && "text-red-600"}`}>0</p>
           <button
             className="px-3 py-2 shadow-neutral-600 border-add-minus shadow button hover:bg-neutral-500 bg-accent-gray border-[1px] rounded flex ml-auto"
             onClick={() => {
@@ -64,21 +131,39 @@ const StockOutModal = ({
         </div>
 
         <div className="py-2 h-[500px] overflow-y-auto flex  flex-col gap-3">
-          {stockouts.map((stockout, index) => (
-            <StockoutForm
-              key={index}
-              stockout={stockout}
-              index={index}
-              handleDelete={removeStockout}
-            />
-          ))}
+          {stockoutData.map((data, index) => {
+            return (
+              <StockoutForm
+                grade={grade} // TODO: Check if this is the correct type,
+                key={index}
+                stockoutData={data}
+                index={index}
+                handleDelete={removeStockout}
+                handleUpdate={handleUpdate}
+              />
+            );
+          })}
         </div>
-        {isError && (
-          <p className="flex justify-center items-center text-red-500">
-            <BiError className=" h-[40px] w-[40px] mr-4" /> Sorted Quantity does
-            not match the ungraded quantity.
-          </p>
-        )}
+        <div className="">
+          {isError.grade && (
+            <p className="flex justify-center items-center text-red-500">
+              <BiError className=" h-[20px] w-[20px] mr-4" /> Please choose
+              grade or remove the form without grade.
+            </p>
+          )}
+          {isError.quantity && (
+            <p className="flex justify-center items-center text-red-500">
+              <BiError className=" h-[20px] w-[20px] mr-4" /> Stockout quantity
+              is greater than the stock quantity.
+            </p>
+          )}
+          {isError.stock && (
+            <p className="flex justify-center items-center text-red-500">
+              <BiError className=" h-[20px] w-[20px] mr-4" /> Stock doesn't
+              exist.
+            </p>
+          )}
+        </div>
         <div className="py-4 flex gap-3 justify-center">
           <button
             className="bg-red-500 text-white float-right min-w-[100px] rounded-full px-4 py-2"
@@ -90,7 +175,9 @@ const StockOutModal = ({
           </button>
           <button
             className="bg-primary-color text-white float-right min-w-[100px] rounded-full px-4 py-2"
-            onClick={() => {}}
+            onClick={() => {
+              handleSubmit();
+            }}
           >
             Save
           </button>
