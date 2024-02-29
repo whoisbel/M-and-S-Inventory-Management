b75c17fedeb6677e50397ee5bb7892fe94b778b1
whoisbel <mjabelcina@gmail.com>
2024-03-01 03:43:29 +0800

    Refactor register.input.validation

--- cypress/e2e/register.input.validation.cy.ts
+++ cypress/e2e/register.input.validation.cy.ts
@@ -1,251 +1,80 @@
-
 interface SecurityQuestion {
-    id: number;
-    question: string;
+  id: number;
+  question: string;
+}
+
+const fillForm = (firstName: string, lastName: string, username: string, password: string, confirmPassword: string, answers: string[]) => {
+
+  if(firstName){
+    cy.get('input[placeholder="First name"]').type(firstName);
   }
-  
-  describe('Return error due to invalid or blank inputs', () => {
-    it("should open a swal modal due to invalid input in first name",() => {
-      cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
-      cy.visit('http://localhost:3000');
-      cy.wait('@getRegistrationData');
-      cy.get('p').contains('Create account here').click();
-      cy.get('p.top-text').should('have.text', 'Create Account');
-      cy.get('.register-modal').within(() => {
-        cy.get('input[placeholder="First name"]').type('John12');
-        cy.get('input[placeholder="Last name"]').type('Doe');
-        cy.get('input[placeholder="Username"]').type('johndoe123');
-        cy.get('input[placeholder="Enter password"]').type('john123');
-        cy.get('input[placeholder="Confirm password"]').type('john123');
-        const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
-        const desiredQuestions = securityQuestions.filter(question => {
-          return question.question === "What is your favorite color?" ||
-                 question.question === "What is your favorite food?" ||
-                 question.question === "What is your mother's maiden name?";
-        });
-        cy.get('select').each(($select, index) => {
-          const question = desiredQuestions[index];
-          if (question) {
-            cy.wrap($select).select(question.id.toString());
-          }
-        });
-        cy.get('div.questions-input').each(($div, divIndex) => {
-          cy.wrap($div).within(() => {
-            cy.get('input').each(($input, inputIndex) => {
-              const answers = [
-                "Red",
-                "Spaghetti",
-                "Jane Doe"
-              ];
-              cy.wrap($input).type(answers[divIndex]);
-            });
-          });
-        });
-        cy.get('button').should('have.text', 'Request').click()
-      });
-      cy.get('div.swal2-popup').within(() => {
-        cy.get('div.swal2-html-container').should('have.text', 'Please enter a valid first name')
-      })
-    });
-    it("should open a swal modal due to blank input in first name",() => {
-      cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
-      cy.visit('http://localhost:3000');
-      cy.wait('@getRegistrationData');
-      cy.get('p').contains('Create account here').click();
-      cy.get('p.top-text').should('have.text', 'Create Account');
-      cy.get('.register-modal').within(() => {
-        cy.get('input[placeholder="Last name"]').type('Doe');
-        cy.get('input[placeholder="Username"]').type('johndoe123');
-        cy.get('input[placeholder="Enter password"]').type('john123');
-        cy.get('input[placeholder="Confirm password"]').type('john123');
-        const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
-        const desiredQuestions = securityQuestions.filter(question => {
-          return question.question === "What is your favorite color?" ||
-                 question.question === "What is your favorite food?" ||
-                 question.question === "What is your mother's maiden name?";
-        });
-        cy.get('select').each(($select, index) => {
-          const question = desiredQuestions[index];
-          if (question) {
-            cy.wrap($select).select(question.id.toString());
-          }
-        });
-        cy.get('div.questions-input').each(($div, divIndex) => {
-          cy.wrap($div).within(() => {
-            cy.get('input').each(($input, inputIndex) => {
-              const answers = [
-                "Red",
-                "Spaghetti",
-                "Jane Doe"
-              ];
-              cy.wrap($input).type(answers[divIndex]);
-            });
-          });
-        });
-        cy.get('button').should('have.text', 'Request').click()
-      });
-      cy.get('div.swal2-popup').within(() => {
-        cy.get('div.swal2-html-container').should('have.text', 'Please enter a valid first name')
-      })
-    });
-    it("should open a swal modal due to invalid input in last name",() => {
-      cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
-      cy.visit('http://localhost:3000');
-      cy.wait('@getRegistrationData');
-      cy.get('p').contains('Create account here').click();
-      cy.get('p.top-text').should('have.text', 'Create Account');
-      cy.get('.register-modal').within(() => {
+  if(lastName){
+    cy.get('input[placeholder="Last name"]').type(lastName);
+  }
+  if(username){
+    cy.get('input[placeholder="Username"]').type(username);
+  }
+  if(password){
+    cy.get('input[placeholder="Enter password"]').type(password);
+  }
+  if(confirmPassword){
+    cy.get('input[placeholder="Confirm password"]').type(confirmPassword);
+  }
 
-        cy.get('input[placeholder="First name"]').type('John');
-        cy.get('input[placeholder="Last name"]').type('Doe2');
-        cy.get('input[placeholder="Username"]').type('johndoe123');
-        cy.get('input[placeholder="Enter password"]').type('john123');
-        cy.get('input[placeholder="Confirm password"]').type('john123');
-        const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
-        const desiredQuestions = securityQuestions.filter(question => {
-          return question.question === "What is your favorite color?" ||
-                 question.question === "What is your favorite food?" ||
-                 question.question === "What is your mother's maiden name?";
-        });
-        cy.get('select').each(($select, index) => {
-          const question = desiredQuestions[index];
-          if (question) {
-            cy.wrap($select).select(question.id.toString());
-          }
-        });
-        cy.get('div.questions-input').each(($div, divIndex) => {
-          cy.wrap($div).within(() => {
-            cy.get('input').each(($input, inputIndex) => {
-              const answers = [
-                "Red",
-                "Spaghetti",
-                "Jane Doe"
-              ];
-              cy.wrap($input).type(answers[divIndex]);
-            });
-          });
-        });
-        cy.get('button').should('have.text', 'Request').click()
-      });
-      cy.get('div.swal2-popup').within(() => {
-        cy.get('div.swal2-html-container').should('have.text', 'Please enter a valid last name')
-      })
-    });
-    it("should open a swal modal due to blank input in last name",() => {
-      cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
-      cy.visit('http://localhost:3000');
-      cy.wait('@getRegistrationData');
-      cy.get('p').contains('Create account here').click();
-      cy.get('p.top-text').should('have.text', 'Create Account');
-      cy.get('.register-modal').within(() => {
 
-        cy.get('input[placeholder="First name"]').type('John');
-        cy.get('input[placeholder="Username"]').type('johndoe123');
-        cy.get('input[placeholder="Enter password"]').type('john123');
-        cy.get('input[placeholder="Confirm password"]').type('john123');
-        const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
-        const desiredQuestions = securityQuestions.filter(question => {
-          return question.question === "What is your favorite color?" ||
-                 question.question === "What is your favorite food?" ||
-                 question.question === "What is your mother's maiden name?";
-        });
-        cy.get('select').each(($select, index) => {
-          const question = desiredQuestions[index];
-          if (question) {
-            cy.wrap($select).select(question.id.toString());
-          }
-        });
-        cy.get('div.questions-input').each(($div, divIndex) => {
-          cy.wrap($div).within(() => {
-            cy.get('input').each(($input, inputIndex) => {
-              const answers = [
-                "Red",
-                "Spaghetti",
-                "Jane Doe"
-              ];
-              cy.wrap($input).type(answers[divIndex]);
-            });
-          });
-        });
-        cy.get('button').should('have.text', 'Request').click()
-      });
-      cy.get('div.swal2-popup').within(() => {
-        cy.get('div.swal2-html-container').should('have.text', 'Please enter a valid last name')
-      })
-    });
-    it("should open a swal modal due to password and confirm password being not the same",() => {
-      cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
-      cy.visit('http://localhost:3000');
-      cy.wait('@getRegistrationData');
-      cy.get('p').contains('Create account here').click();
-      cy.get('p.top-text').should('have.text', 'Create Account');
-      cy.get('.register-modal').within(() => {
+  const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
+  const desiredQuestions = securityQuestions.filter(question => {
+    return question.question === "What is your favorite color?" ||
+           question.question === "What is your favorite food?" ||
+           question.question === "What is your mother's maiden name?";
+  });
 
-        cy.get('input[placeholder="First name"]').type('John');
-        cy.get('input[placeholder="Last name"]').type('Doe');
-        cy.get('input[placeholder="Username"]').type('johndoe123');
-        cy.get('input[placeholder="Enter password"]').type('john123');
-        cy.get('input[placeholder="Confirm password"]').type('john1232');
-        const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
-        const desiredQuestions = securityQuestions.filter(question => {
-          return question.question === "What is your favorite color?" ||
-                 question.question === "What is your favorite food?" ||
-                 question.question === "What is your mother's maiden name?";
-        });
-        cy.get('select').each(($select, index) => {
-          const question = desiredQuestions[index];
-          if (question) {
-            cy.wrap($select).select(question.id.toString());
-          }
-        });
-        cy.get('div.questions-input').each(($div, divIndex) => {
-          cy.wrap($div).within(() => {
-            cy.get('input').each(($input, inputIndex) => {
-              const answers = [
-                "Red",
-                "Spaghetti",
-                "Jane Doe"
-              ];
-              cy.wrap($input).type(answers[divIndex]);
-            });
-          });
-        });
-        cy.get('button').should('have.text', 'Request').click()
-      });
-      cy.get('div.swal2-popup').within(() => {
-        cy.get('div.swal2-html-container').should('have.text', 'Passwords do not match')
-      })
-    });
-    it("should open a swal modal due to security questions not answered",() => {
-      cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
-      cy.visit('http://localhost:3000');
-      cy.wait('@getRegistrationData');
-      cy.get('p').contains('Create account here').click();
-      cy.get('p.top-text').should('have.text', 'Create Account');
-      cy.get('.register-modal').within(() => {
+  cy.get('select').each(($select, index) => {
+    const question = desiredQuestions[index];
+    if (question) {
+      cy.wrap($select).select(question.id.toString());
+    }
+  });
 
-        cy.get('input[placeholder="First name"]').type('John');
-        cy.get('input[placeholder="Last name"]').type('Doe');
-        cy.get('input[placeholder="Username"]').type('johndoe123');
-        cy.get('input[placeholder="Enter password"]').type('john123');
-        cy.get('input[placeholder="Confirm password"]').type('john1232');
-        const securityQuestions: SecurityQuestion[] = require('../fixtures/registrationData.json').securityQuestions;
-        const desiredQuestions = securityQuestions.filter(question => {
-          return question.question === "What is your favorite color?" ||
-                 question.question === "What is your favorite food?" ||
-                 question.question === "What is your mother's maiden name?";
-        });
-        cy.get('select').each(($select, index) => {
-          const question = desiredQuestions[index];
-          if (question) {
-            cy.wrap($select).select(question.id.toString());
-          }
-        });
-        cy.get('button').should('have.text', 'Request').click()
+  cy.get('div.questions-input').each(($div, divIndex) => {
+    cy.wrap($div).within(() => {
+      cy.get('input').each(($input, inputIndex) => {
+        if (answers[divIndex]) {
+          cy.wrap($input).type(answers[divIndex]);
+        }
       });
-      cy.get('div.swal2-popup').within(() => {
-        cy.get('div.swal2-html-container').should('have.text', 'Please enter a valid security question answer')
-      })
     });
   });
-  
+
+  cy.get('button').should('have.text', 'Request').click()
+}
+
+describe('Return error due to invalid or blank inputs', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/auth/register', { fixture: 'registrationData.json' }).as('getRegistrationData');
+    cy.visit('http://localhost:3000');
+    cy.wait('@getRegistrationData');
+    cy.get('p').contains('Create account here').click();
+    cy.get('p.top-text').should('have.text', 'Create Account');
+  });
+
+  const testCases = [
+    {firstName: 'John12', lastName: 'Doe', username: 'johndoe123', password: 'john123', confirmPassword: 'john123', answers: ["Red", "Spaghetti", "Jane Doe"], errorMessage: 'Please enter a valid first name'},
+    {firstName: '', lastName: 'Doe', username: 'johndoe123', password: 'john123', confirmPassword: 'john123', answers: ["Red", "Spaghetti", "Jane Doe"], errorMessage: 'Please enter a valid first name'},
+    {firstName: 'John', lastName: 'Doe2', username: 'johndoe123', password: 'john123', confirmPassword: 'john123', answers: ["Red", "Spaghetti", "Jane Doe"], errorMessage: 'Please enter a valid last name'},
+    {firstName: 'John', lastName: '', username: 'johndoe123', password: 'john123', confirmPassword: 'john123', answers: ["Red", "Spaghetti", "Jane Doe"], errorMessage: 'Please enter a valid last name'},
+    {firstName: 'John', lastName: 'Doe', username: 'johndoe123', password: 'john123', confirmPassword: 'john1232', answers: ["Red", "Spaghetti", "Jane Doe"], errorMessage: 'Passwords do not match'},
+    {firstName: 'John', lastName: 'Doe', username: 'johndoe123', password: 'john123', confirmPassword: 'john123', answers: [], errorMessage: 'Please enter a valid security question answer'},
+  ];
+
+  testCases.forEach(({firstName, lastName, username, password, confirmPassword, answers, errorMessage}) => {
+    it(`should open a swal modal with message "${errorMessage}"`, () => {
+      cy.get('.register-modal').within(() => {
+        fillForm(firstName, lastName, username, password, confirmPassword, answers);
+      });
+      cy.get('div.swal2-popup').within(() => {
+        cy.get('div.swal2-html-container').should('have.text', errorMessage)
+      });
+    });
+  });
+});
