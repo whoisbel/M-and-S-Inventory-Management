eee60284722b1b6a9ef7fa8fd24953da4977130d
whoisbel <mjabelcina@gmail.com>
2024-03-07 23:19:42 +0800

    Implemented the search bar feature

--- app/admin_mngt/users_mngt/manage_users/page.tsx
+++ app/admin_mngt/users_mngt/manage_users/page.tsx
@@ -5,6 +5,8 @@ import { User } from "@prisma/client";
 
 const ManageUsers = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     async function fetchUsers() {
       const response = await fetch("/api/admin_mngt/users_mngt/manage_users");
@@ -15,17 +17,32 @@ const ManageUsers = () => {
     }
     fetchUsers();
   }, []);
+
+  const handleSearch = (searchTerm: string) => {
+    setSearchTerm(searchTerm);
+  };
+
+  let filteredUsers = users;
+  if (searchTerm) {
+    const searchWords = searchTerm.toLowerCase().split(" ");
+    filteredUsers = users.filter((user) => {
+      const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+      return searchWords.every((word) => fullName.includes(word));
+    });
+  }
+
   return (
     <div className="ml-3 pt-4 px-4 max-h-[750px] h-full w-full border border-add-minus rounded-lg">
       <div className="flex justify-between mb-5">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
       </div>
       <div className=" flex flex-col">
-        <span className="text-[20px] bold border-b-4  border-primary-color">Manage Users</span>
+        <span className="text-[20px] bold border-b-4  border-primary-color">
+          Manage Users
+        </span>
         <div className="overflow-auto max-h-[550px] w-full">
-        <AdminTable users={users} />
+          <AdminTable users={filteredUsers} />
         </div>
-        
       </div>
     </div>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,34 @@
-import { BiSearch } from "react-icons/bi"
+"use client";
+import React, { useState } from "react";
+import { BiSearch } from "react-icons/bi";
 
-const SearchBar = () => {
-  return (
-    <div className="flex-1 flex items-center ">
-    <input
-      type="text" 
-      name="search"
-      id="search"
-      placeholder="Search"
-      className="border border-a rounded-lg  h-[30px] pl-2"
-    />
-    <BiSearch className="-ml-[1.25em] text-primary-color" />
-  </div>
-  )
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
 }
 
-export default SearchBar
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+    onSearch(newSearchTerm);
+  };
+
+  return (
+    <div className="flex-1 flex items-center ">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search"
+        className="border border-a rounded-lg  h-[30px] pl-2"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <BiSearch className="-ml-[1.25em] text-primary-color" />
+    </div>
+  );
+};
+
+export default SearchBar;
