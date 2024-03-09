69f4e307518ecfca0c16ba037e44a87ba8777bb4
unknown <j.canedo.527294@umindanao.edu.ph>
2024-03-09 12:02:10 +0800

    user mngmt

--- app/api/admin_mngt/users_mngt/manage_users/route.ts
+++ app/api/admin_mngt/users_mngt/manage_users/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
+import { Event, Venue } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
 export async function GET(req: NextRequest) {
   const users = await prisma.user.findMany({
@@ -15,4 +18,22 @@ export async function GET(req: NextRequest) {
     },
   });
   return NextResponse.json(users);
+}
+
+export async function DELETE(req: NextRequest) {
+  const { user } = await req.json();
+  const session = await getServerSession(options);
+  await prisma.$transaction([
+    prisma.user.delete({
+      where: { id: user.id },
+    }),
+    prisma.actionLog.create({
+      data: {
+        venue: Venue.manageUsers,
+        event: Event.delete,
+        userId: session!.user.id!,
+      },
+    }),
+  ]);
+  return NextResponse.json({ message: "User deleted" }, { status: 200 });
 }
